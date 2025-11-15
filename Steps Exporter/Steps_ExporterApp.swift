58fca4d818e02410e61067e0f707eff760333278
Philipp Gaßner <activator112233@gmail.com>
2025-11-15 14:24:33 +0100

    Initial Commit

---
+++ Steps Exporter/Steps_ExporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Steps_ExporterApp.swift
+//  Steps Exporter
+//
+//  Created by rillo on 15.11.25.
+//
+
+import SwiftUI
+
+@main
+struct Steps_ExporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
